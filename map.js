93f63364497fac1e28ba72a1d996b84412949bb1
km322 <mittal.ketan1@gmail.com>
2025-05-11 12:30:09 -0700

    added map

---
+++ map.js
@@ -0,0 +1,14 @@
+import mapboxgl from "https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm";
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoiazFtaXR0YWwiLCJhIjoiY21hazBwMzd1MDNtZjJtcHdtdThnd2NwZyJ9.vB6tJ9iM97k4k2vyiopwiw';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
